ad49d6eb2ef7fc87b143a785d6f63c4850e13332
SalmanHossen <hossainsalman873@gmail.com>
2025-11-05 23:47:35 +0600

    create port and run the app smothly

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './src/config/database.config.js';
 
 dotenv.config();
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+connectDB()
 
 export default app;
