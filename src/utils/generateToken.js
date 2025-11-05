c51a143ba97d2176a734d228480ec8aa5369de47
SalmanHossen <hossainsalman873@gmail.com>
2025-11-06 00:15:38 +0600

    create generate token file and export

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,20 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken=(res,userID)=>{
+const generateToken=(req,res,userID)=>{
    try {
-      const token=jwt.sign({userId},process.env.)
+      const token=jwt.sign({userID},process.env.JWT_SECRET);
+      expiresIn:'30d';
+
+      res.cookie('jwt',token,{
+         httpOnly:true,
+         secure:process.env.Node_ENV!=='development',
+         sameSite:'strict',
+         maxAge:30*24*60*60*1000
+      });
+
    } catch (error) {
-      
+      console.log(`Server error for generate token : ${error.message}`);
    }
 }
+
+export default generateToken;
