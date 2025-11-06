a688d622be3e7d6d8cf10e790b0bb3073ccd4dfc
SalmanHossen <hossainsalman873@gmail.com>
2025-11-06 14:41:51 +0600

    update profile controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -56,7 +56,8 @@ export const login=async(req, res)=>{
             message:'Invalid email or password',
          })
       }
-      const validPassword=await bcrypt.compare(password,user.password);
+      
+      const validPassword=await bcrypt.compare(password,user.password,10);
 
       if(validPassword){
 
@@ -79,6 +80,60 @@ export const login=async(req, res)=>{
       res.status(500).json({
          message:'Server error occur during login',
          error:error.message,
+      })
+   }
+}
+
+export const updateProfile=async(req , res)=>{
+   try {
+      const user=await User.findById(req.user._id);
+      
+      if(!user){
+         return res.status(404).json({
+            message:'User not found'
+         })
+      }
+      user.name=req.body.name ||user.name;
+
+      const newEmail=req.body.email;
+
+      if(newEmail && newEmail !==user.email){
+
+         const emailExists=await User.findOne({email:newEmail});
+
+         if(emailExists){
+            return res.status(400).json({
+               message:'This email already taken'
+            });
+         }
+         user.email=newEmail;
+      }
+
+      user.phoneNumber=req.body.phoneNumber || user.phoneNumber;
+
+      const newPassword=req.body.password;
+      if(newPassword){
+
+         const salt=await bcrypt.genSalt(10);
+         user.password= await bcrypt.hash(newPassword,salt);
+         
+      }
+      const updateUser=await user.save();
+
+      return res.status(200).json({
+         message:"User updated successfully",
+         data:{
+            _id:updateUser._id,
+            name:updateUser.name,
+            email:updateUser.email,
+            phoneNumber:updateUser.phoneNumber,
+         }
+      });
+
+   } catch (error) {
+      res.status(500).json({
+         message:'Server error occur during update profile',
+         error:error.message
       })
    }
 }
