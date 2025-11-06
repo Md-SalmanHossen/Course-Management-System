91c017d3856ea76ed964c0030bbf7e7e4ca5dc5c
SalmanHossen <hossainsalman873@gmail.com>
2025-11-06 12:30:32 +0600

    route not found handler use as a middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/database.config.js';
+import routeNotFound from './src/middlewares/route_not_found.middleware.js';
 
 dotenv.config();
 const app=express();
@@ -9,5 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 connectDB()
+
+
+app.use(routeNotFound)
 
 export default app;
