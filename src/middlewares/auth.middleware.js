e3bf84495865b845b432fb0528db46bece48c886
SalmanHossen <hossainsalman873@gmail.com>
2025-11-06 10:58:34 +0600

    create auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,13 +5,23 @@ const protect=async(req ,res , next)=>{
    try {
       let token=req.cookies.jwt;
 
-      if(!token){
+      if(token){
          const decode=jwt.verify(token, process.env.JWT_SECRET);
-         req.user=await User.findById(decode.userId).select('_password');
+         req.user=await User.findById(decode.userId).select('-password');
          next();
+
+      }else{
+         return res.status(401).json({
+            message:"Not Authorized,no token"
+         })
       }
       
    } catch (error) {
-      
+      res.status(500).json({
+         message:"Server error in auth middleware",
+         error:error.message,
+      })
    }
 }
+
+export default protect;
